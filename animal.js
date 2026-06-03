e1581a151d8dd7f3145b9f2129cc4ed98237fce2
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-06-03 13:53:38 +0000

    v1

--- animal.js
+++ animal.js
@@ -1,1 +1,11 @@
+const animal = {
+    nome: "Tchutchuca", //strig
+    especie: "pinguim",
+    idade: 2, //number
+    tutor: false,
+    vacinado: true
 
+
+};
+//console.log(animal);
+console.log(animal.nome);
