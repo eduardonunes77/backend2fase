de08487a9adb74db7295b7157c554a5aebd2b5fb
eduardonunes77 <eduardo.nunes.silva11@escola.pr.gov.br>
2026-06-29 13:54:50 +0000

    V2

--- curtidas.js
+++ curtidas.js
@@ -2,11 +2,12 @@ const feedJSON = require('./feed.json');
 
 //Adicionar uma curtida ao post pelo id
 function curtir(feed, id) {
-    const post = feed[0].id === id ? feed[0] : null;
+    const post = feed[0].id === id ? feed[0] : 
+        feed[1].id === id ? feed[1] : feed[2];
 }
 
-if (!post) {
-    console.log("Post não encontrado.");
+if (!post.metricas.curtidas === 0) {
+    console.log("Este post já está com 0️⃣ curtidas.");
     return;
 }
 
@@ -17,3 +18,19 @@ if (!post.publicado) {
 
 post.metricas.curtidas++;
 console.log(`Post de @${post.autor} agora tem ${post.metricas.curtidas} curtidas.`);
+
+//Remove uma curtida - não deixa ir abaixo de zero
+function descurtir(feed, id) {
+    const post = feed[0].id === id ? feed[0] :
+                feed[1].id === id ? feed[1] : feed[2];
+}
+
+if(post.metricas.curtidas === 0){
+    console.log("Este post já está com 0 curtidas.");
+    return;
+}
+
+post.metricas.curtidas++;
+console.log(`💔 Curtidas removida. Total ${post.metricas.curtidas}`);
+
+curtir(feedJSON, 1);
